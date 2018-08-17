eae5247478376bc878bdd15989657ded13634ba1
Nixie Skywalker <nyxscream@gmail.com>
2018-08-17 11:55:45 +0200

    create note route and note list route

--- routes/notes.js
+++ routes/notes.js
@@ -2,26 +2,41 @@
 
 const express = require('express');
 const router = express.Router();
+const ObjectId = require('mongodb').ObjectID;
 
 const Note = require('../models/note');
 
-router.get('/notes', (req, res, next) => {
+router.get('/', (req, res, next) => {
+    const oid = req.session.currentUser._id;
+    Note.find({'owner': ObjectId(oid)})
+        .then(notes => {
+            res.json(notes);
+        })
+        .catch(error => next(error));
+});
+
+router.get('/:id', (req, res, next) => {
+    
+});
+
+router.post('/', (req, res, next) => {
+    const data = req.body;
+    const title = data[0];
+    const content = data.slice(1).join('\n');
+    const owner = req.session.currentUser._id;
+    Note.create({
+        owner,
+        title,
+        content
+    });
+    res.status(200).send();
+});
+
+router.put('/:id', (req, res, next) => {
 
 });
 
-router.get('/notes/:id', (req, res, next) => {
-
-});
-
-router.post('/notes', (req, res, next) => {
-
-});
-
-router.put('/notes/:id', (req, res, next) => {
-
-});
-
-router.delete('/notes/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
 
 });
 
