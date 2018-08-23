47d95d1d683e171cc17bbfb6c809f39d8b7bc509
Nixie Skywalker <nyxscream@gmail.com>
2018-08-23 14:37:47 +0200

    sdlkfhlsd

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ io.configure(function () {
     io.set("transports", ["xhr-polling"]); 
     io.set("polling duration", 10); 
 });
+
 const port = process.env.PORT || 8080;
 app.listen(port, function() {  
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
