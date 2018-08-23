df56af5a17cfd936ff6d44ce532dc7286cc46e55
Nixie Skywalker <nyxscream@gmail.com>
2018-08-23 13:12:28 +0200

    sjfdlsjflfd

--- app.js
+++ app.js
@@ -42,26 +42,16 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-
-    // Website you wish to allow to connect
     res.header('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', false);
 
-    // Pass to next layer of middleware
     next();
 });
 
 
-const server = app.listen(cross.NormalizePort(process.env.PORT || 8080));
+const server = app.listen(rocess.env.PORT || 8080);
 const io = require('socket.io').listen(server, {
     log: false,
     agent: false,
