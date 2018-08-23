5f99f8b7ce28878fe5c336e1bee7ae0b0328a05d
Nixie Skywalker <nyxscream@gmail.com>
2018-08-23 13:15:12 +0200

    jdjdsf

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 });
 
 
-const server = app.listen(rocess.env.PORT || 8080);
+const server = app.listen(process.env.PORT || 8080);
 const io = require('socket.io').listen(server, {
     log: false,
     agent: false,
