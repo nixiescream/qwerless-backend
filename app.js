7a39fa88f2dbccdf6d3afc45ac1e52543fffe114
Nixie Skywalker <nyxscream@gmail.com>
2018-08-23 12:20:14 +0200

    lkdjsj

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const authRouter = require('./routes/auth');
 const notesRouter = require('./routes/notes');
 
 const app = express();
-const io = require('socket.io')(process.env.PORT || 8080);
+// const io = require('socket.io')(process.env.PORT || 8080);
+const io = require('socket.io')(8080);
 
 
 app.use(logger('dev'));
